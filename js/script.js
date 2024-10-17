3a8b275b2463e54e445e43e105c95a83184238e0
eli _ <metall65eli@gmail.com>
2024-10-17 19:51:44 -0300

    details

--- js/script.js
+++ js/script.js
@@ -7,26 +7,9 @@ var boardConfig = {
 var board1 = ChessBoard('board1', boardConfig);
 var game = new Chess();
 
-function makeRandomMove() {
-  var possibleMoves = game.moves();
 
-  // Exit if the game is over
-  if (game.game_over() === true || possibleMoves.length === 0) return;
-
-  // Pick a random move from possible moves
-  var randomMove = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
-  
-  // Make the random move
-  game.move(randomMove);
-  
-  // Update the board with the new position
-  board1.position(game.fen());
-  
-  // Update the move history
-  updateMoveHistory(game.history({ verbose: true }).slice(-1)[0]);
-}
 function makeEngineMove() {
-  var bestMove = ChessEngine.getBestMove(3, game); // Set depth to 3 (adjust as needed)
+  var bestMove = ChessEngine.getBestMove(10, game); 
   
   if (bestMove !== null) {
     game.move(bestMove);
@@ -38,11 +21,11 @@ function handleMove(source, target, piece, newPos, oldPos, orientation) {
   var move = game.move({
     from: source,
     to: target,
-    promotion: 'q' // always promote to queen for simplicity
+    promotion: 'q' 
   });
 
   if (move === null) {
-    return 'snapback'; // illegal move
+    return 'snapback'; 
   }
 
   updateMoveHistory(move);
@@ -52,7 +35,7 @@ function handleMove(source, target, piece, newPos, oldPos, orientation) {
   }, 500);
 }
 
-// Update the board position after the piece snap for castling, etc.
+
 function onSnapEnd() {
   board1.position(game.fen());
 }
@@ -67,26 +50,24 @@ function updateMoveHistory(move) {
 function checkGameStatus() {
   if (game.in_checkmate()) {
     if (game.turn() === 'w') {
-      alert("Checkmate! You lost."); // Opponent (engine) made the checkmate
+      alert("JAQUE MATE, PERDISTE."); 
     } else {
-      alert("Checkmate! You won!"); // User made the checkmate
+      alert("JAQUE MATE, GANASTE"); 
     }
     resetGame();
   } else if (game.in_draw()) {
-    alert("Draw! The game ended in a draw.");
+    alert("tablas! La partida termino en tablas ");
     resetGame();
   } else if (game.in_stalemate()) {
-    alert("Stalemate! The game ended in a stalemate.");
+    alert("Ahogado, La partida termino en ahogado");
     resetGame();
   } else if (game.in_threefold_repetition()) {
-    alert("Draw! Threefold repetition.");
+    alert("Tablas, La partida termino en tablas");
     resetGame();
   } else if (game.insufficient_material()) {
-    alert("Draw! Insufficient material to continue.");
+    alert("Tablas, La partida termino por triple repeticion");
     resetGame();
-  } else if (game.in_check()) {
-    alert("Check!");
-  }
+  } 
 }
 
 function resetGame() {
